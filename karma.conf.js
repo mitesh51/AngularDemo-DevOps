fe48863ca46c7826ea65ba6dcc2bbbcbf4c3acbf
mitesh51 <mitesh.soni83@gmail.com>
2020-11-17 19:14:00 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,15 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+ browsers: ['MyChromeHeadless'], // <- not 'ChromeHeadless'
+customLaunchers: {
+ MyChromeHeadless: {
+    base: 'ChromeHeadless',
+    flags: [
+      '--no-sandbox'
+    ]
+  }
+}
     singleRun: false,
     restartOnFileChange: true
   });
